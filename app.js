ec2190e48eacabe4bed79d3e7e8e1bc214ee0664
vamsikommireddi <vamsikrishna.kommireddi@gmail.com>
2016-07-30 18:40:03 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3000, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+appId: '261376bc-f2da-4ddf-9968-b7f18d949a18',
+appPassword: 'HKCfk2NVZ1yOAsreFNUnSyY'
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
